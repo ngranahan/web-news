19ec8f24130d3d7bcb3292b54b1f5247177f9558
Nora Granahan <nora@punkave.com>
2018-06-20 07:27:16 -0400

    Updated mongo connection for deployment

--- server.js
+++ server.js
@@ -19,6 +19,11 @@ const routes = require("./controllers/newsController.js");
 app.use(routes);
 
 // Connection to MongoDB
-mongoose.connect("mongodb://localhost/webnewsdb");
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/webnewsdb";
+
+// Set mongoose to leverage built in JavaScript ES6 Promises
+// Connect to the Mongo DB
+mongoose.Promise = Promise;
+mongoose.connect(MONGODB_URI);
 
 app.listen(PORT, () => console.log("App running on port " + PORT + "!"));
